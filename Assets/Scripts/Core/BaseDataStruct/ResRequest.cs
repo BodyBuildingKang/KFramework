1a8087945ed80d97d7da38f5a70207af95f05bea
kangzhentian <kangzhentian@bettagames.com>
2023-03-08 11:43:49 +0800

    资源(ResRequest 单位)请求数据结构添加

--- Assets/Scripts/Core/BaseDataStruct/ResRequest.cs
+++ Assets/Scripts/Core/BaseDataStruct/ResRequest.cs
@@ -1,5 +1,5 @@
- /** 
-*    Class Description: 资源请求数据结构
+/** 
+*    Class Description: 资源(ResRequest 单位)请求数据结构
 *
 *    CreateTime: 2023-03-07 14:35:33
 *
@@ -7,14 +7,89 @@
 *
 */
 
-using System.Collections;
+ using System;
+ using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+ using Unity.VisualScripting;
+ using UnityEditor.PackageManager.Requests;
+ using UnityEngine;
 
 namespace kAsset
 {
-      public class ResRequest
+      public class ResRequest : IEnumerator
       {
+            public enum Result {
+                Default,
+                Success,
+                Failed,
+                Cancelled
+            }
+
+            public enum Status {
+                Wait,
+                Processing,
+                Complete
+            }
+
+            public Action<ResRequest> completed;
+            public Result result { get; protected set; } = Result.Default;
+            public Status status { get; protected set; } = Status.Wait;
+            public bool isDone => status == Status.Complete;
+            public float propress { get; set; }
+            public string error { get; protected set; }
+            public object Current => null;
             
-      }
-}
+            public bool MoveNext(){ return !isDone; }
+
+            public void Reset()
+            {
+                completed = null;
+                status = Status.Wait;
+            }
+
+            public void Start()
+            {
+                if (status != Status.Wait) return;
+                status = Status.Processing;
+                OnStart();
+            }
+            
+            public bool Update()
+            {
+                if (isDone) return false;
+                OnUpdated();
+                return true;
+            }
+
+            public void Completed()
+            {
+                OnCompleted();
+                var saved = completed;
+                completed?.Invoke(this);
+                completed -= saved;
+            }
+
+            public void SetResult(Result value, string msg = null)
+            {
+                propress = 1;
+                result = value;
+                status = result == Result.Default ? Status.Wait : Status.Complete;
+                error = msg;
+            }
+
+            protected virtual void OnStart(){}
+            protected virtual void OnUpdated(){}
+            protected virtual void OnCompleted(){}
+
+            public void SendRequest()
+            {
+                // Scheduler.Enqueue(this);
+            }
+
+            public void Cancel()
+            {
+                SetResult(Result.Cancelled);
+            }
+            
+      }//class_end
+}//namespace_end
